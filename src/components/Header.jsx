da46117adcfe98dc099bd5a044ddd9144ee13d9d
Abheesht Srivastava <Abheesht.Srivastava@ibm.com>
2024-01-17 16:12:40 +0530

    created cart context

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import logoImg from '../assets/logo.jpg'
+import CartContext from '../store/CartContext';
+import Button from './UI/Button';
+import { useContext } from 'react';
+
+export default function Header() {
+    const cartCtx = useContext(CartContext);
+    const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item)=> {
+        return totalNumberOfItems+item.quantity;
+    },0)
+    return <header id="main-header">
+        <div id="title">
+            <img src={logoImg} alt="A restaurant" />
+            <h1>React Food</h1>
+        </div>
+        <nav>
+            <Button textOnly>Cart({totalCartItems})</Button>
+        </nav>
+    </header>;
+}
